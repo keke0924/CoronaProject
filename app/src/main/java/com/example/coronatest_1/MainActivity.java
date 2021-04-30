1d87d9107fd51453b29971dc9c27b323ddc479d2
Yujin <keke0924@naver.com>
2021-04-30 22:23:16 +0900

    first commit

---
+++ app/src/main/java/com/example/coronatest_1/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.coronatest_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener{
+
+    Button btn_corona;
+    Button btn_map;
+    Button btn_vac;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedlnstanceState){
+        super.onCreate(savedlnstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_corona = (Button)findViewById(R.id.btn_corona);
+        btn_corona.setOnClickListener(this);
+        btn_map= (Button)findViewById(R.id.btn_map);
+        btn_map.setOnClickListener(this);
+        btn_vac= (Button)findViewById(R.id.btn_vac);
+        btn_vac.setOnClickListener(this);
+
+       /* btn_corona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Present.class);
+                startActivity(intent);
+            }
+        });*/
+    }
+
+
+    @Override
+     public void onClick(View v) {
+        if(v.getId()==R.id.btn_corona){
+            Intent intent1 = new Intent(MainActivity.this, PresentActivity.class);
+            startActivity(intent1);
+        }
+        else if(v.getId()==R.id.btn_map){
+            Intent intent2 = new Intent(MainActivity.this, GMapsActivity.class);
+            startActivity(intent2);
+        }
+        else if(v.getId()==R.id.btn_vac){
+            Intent intent3 = new Intent(MainActivity.this, VacActivity.class);
+            startActivity(intent3);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/coronatest_1/PresentActivity.java
@@ -0,0 +1,14 @@
+package com.example.coronatest_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PresentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_present2);
+    }
+}
