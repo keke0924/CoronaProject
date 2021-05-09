20cfa50facf6fc5156987457f7344d607cd97a38
Yujin <keke0924@naver.com>
2021-05-09 19:15:26 +0900

    first

--- app/src/main/java/com/example/coronatest_1/NMapsActivity.java
+++ app/src/main/java/com/example/coronatest_1/NMapsActivity.java
@@ -1,7 +1,12 @@
 package com.example.coronatest_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,9 +20,10 @@ import com.naver.maps.map.MapView;
 import com.naver.maps.map.OnMapReadyCallback;
 
 
-public class NMapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class NMapsActivity extends AppCompatActivity implements OnMapReadyCallback, OnClickListener {
     private MapView mapView;
     private static NaverMap naverMap;
+    Button btn_now;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,9 @@ public class NMapsActivity extends AppCompatActivity implements OnMapReadyCallba
         mapView = (MapView) findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
+
+        btn_now= (Button)findViewById(R.id.now);
+        btn_now.setOnClickListener(this);
     }
 
     @Override
@@ -44,5 +53,13 @@ public class NMapsActivity extends AppCompatActivity implements OnMapReadyCallba
         );
         naverMap.setCameraPosition(cameraPosition);
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.now){
+            Intent intent1 = new Intent(NMapsActivity.this, now.class);
+            startActivity(intent1);
+        }
     }
 }
---
+++ app/src/main/java/com/example/coronatest_1/now.java
@@ -0,0 +1,16 @@
+package com.example.coronatest_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class now extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now);
+    }
+}
