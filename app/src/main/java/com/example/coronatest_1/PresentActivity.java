58b95d744679728752a875b6d4fd1ce5afccc75f
erica2512 <012banada@naver.com>
2021-05-16 01:41:29 +0900

    그래프 틀

--- app/src/main/java/com/example/coronatest_1/PresentActivity.java
+++ app/src/main/java/com/example/coronatest_1/PresentActivity.java
@@ -2,13 +2,20 @@ package com.example.coronatest_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
 
 public class PresentActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_present2
-        );
+        setContentView(R.layout.activity_present2);
     }
+
 }
